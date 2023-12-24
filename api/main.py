83159a6e403ff73025d5ca655a0e1ca85a1e81e5
varreltantio <snetid90@gmail.com>
2023-12-24 13:33:56 +0700

    delete topic

--- api/main.py
+++ api/main.py
@@ -5,27 +5,6 @@ from flask import Flask, render_template, send_file, redirect
 
 app = Flask(__name__)
 
-def scrape_detail(url):
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-    
-        topic_elemen = soup.find('span', class_='ast-terms-link')
-
-        if topic_elemen:
-            link_elemen = topic_elemen.find('a')
-            
-            if link_elemen:
-                topic = link_elemen.text.strip()
-                return topic
-            else:
-                return None
-        else:
-            return None      
-    else:
-        return None
-
 def scrape_title(url):
     response = requests.get(url)
     
@@ -34,20 +13,16 @@ def scrape_title(url):
         semua_judul_elemen = soup.find_all('h2', class_='entry-title')
 
         judul_list = []
-        topic_list = []
 
         for judul_elemen in semua_judul_elemen[:3]:
             link_elemen = judul_elemen.find('a')
 
             if link_elemen:
                 judul = link_elemen.text.strip()
-                url_tautan = link_elemen['href']
-                topic = scrape_detail(url_tautan)
 
                 judul_list.append(judul)
-                topic_list.append(topic)
 
-        return judul_list, topic_list
+        return judul_list
     else:
         return None, None
 
@@ -63,16 +38,14 @@ def scrape():
     page_number = 1
 
     judul_total = []
-    topic_total = []
 
     current_url = f'{base_url}/page/1'
     print(f'Scraping {current_url}')
         
-    judul, topic = scrape_title(current_url)
+    judul = scrape_title(current_url)
 
-    if judul and topic:
+    if judul:
         judul_total.extend(judul)
-        topic_total.extend(topic)
 
     # while True:
     #     current_url = f'{base_url}/page/{page_number}'
@@ -95,7 +68,7 @@ def scrape():
         
     #     page_number += 1
 
-    df = pd.DataFrame({'Judul': judul_total, 'Topic': topic_total})
+    df = pd.DataFrame({'Judul': judul_total})
     df.to_excel('hasil_scraping.xlsx', index=False)
 
     return redirect('/')
